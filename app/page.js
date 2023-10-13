4fc6c375dfa9efbc9a55476cc30dba78cf5aefcf
Zane Aridi <aridizan@msu.edu>
2023-10-13 15:19:05 -0400

    More description

--- app/page.js
+++ app/page.js
@@ -20,7 +20,10 @@ export default function Home() {
               <p className={styles.mission}>Student Resource Organizations</p>
               <p className={styles.cardText}>
                 We provide custom software development for a variety of clients. 
-                One type of client is student resource organizations.
+                One type of client is student resource organizations. 
+                We haved worked with organizations like the AI Club and Burgess Institute for entrepreneurship & Innovation
+                to help students create software for their business ventures and websites for their clubs. 
+                The photo on the left showcases one of our members who was prseneting the work he did on a mobile app for the company Triple4s.
               </p>
             </div>
           </div>
